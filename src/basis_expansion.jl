6f810ebc0a482359e2aafb8923b2282668e50c2e
Christof Stocker <stocker.christof@gmail.com>
2018-02-06 16:06:35 +0100

    allow missing in expand_poly

--- src/basis_expansion.jl
+++ src/basis_expansion.jl
@@ -1,36 +1,40 @@
 """
     expand_poly(x, [degree = 5], [obsdim]) -> Matrix
 
-Perform a polynomial basis expansion of the given `degree` for
-the vector `x`.
+Perform a simple polynomial basis expansion of the given `degree`
+for the vector `x`.
 
 The optional parameter `obsdim` can be used to specify which
-dimension of the resulting `Matrix` should denote the
-observations. By default the each column will denote an
-observation, which means that the returning `Matrix` will have
-the size `(degree, length(x))`.
+dimension of the resulting `Matrix` should correspond to the
+observations. By default each column will denote an observation,
+which means that the resulting `Matrix` would have the size
+`(degree, length(x))`.
 
-```julia
+```@jldoctest
 julia> expand_poly(1:5, degree = 3)
-3×5 Array{Float64,2}:
+3×5 Array{Int64,2}:
+ 1  2   3   4    5
+ 1  4   9  16   25
+ 1  8  27  64  125
+
+julia> expand_poly(1f0:5f0, 3) # positional arguments are type-stable
+3×5 Array{Float32,2}:
  1.0  2.0   3.0   4.0    5.0
  1.0  4.0   9.0  16.0   25.0
  1.0  8.0  27.0  64.0  125.0
-
-julia> expand_poly(1:5, 3); # same but type-stable
 ```
 
-If you want each row to denote an observation you can set
-`obsdim = 1`.
+Alternatively it is also possible to specify `obsdim = 1`, which
+will cause each row to denote an observation.
 
-```julia
+```@jldoctest
 julia> expand_poly(1:5, degree = 3, obsdim = 1)
-5×3 Array{Float64,2}:
- 1.0   1.0    1.0
- 2.0   4.0    8.0
- 3.0   9.0   27.0
- 4.0  16.0   64.0
- 5.0  25.0  125.0
+5×3 Array{Int64,2}:
+ 1   1    1
+ 2   4    8
+ 3   9   27
+ 4  16   64
+ 5  25  125
 
 julia> expand_poly(1:5, 3, ObsDim.First()); # same but type-stable
 ```
@@ -43,27 +47,23 @@ function expand_poly(x::AbstractVector, degree::Integer, ::ObsDim.Last)
     expand_poly(x, degree, ObsDim.Constant{2}())
 end
 
-function expand_poly{T<:Number}(x::AbstractVector{T}, degree::Integer, ::ObsDim.Constant{2} = ObsDim.Constant{2}())
+function expand_poly(x::AbstractVector{T}, degree::Integer, ::ObsDim.Constant{2} = ObsDim.Constant{2}()) where {T}
     n = length(x)
-    # FIXME: consider support for colorants (i.e. don't hardcode Float64)
-    x_vec = convert(Vector{Float64}, x)
-    X = zeros(Float64, (degree, n))
-    @inbounds for i = 1:n
-        for d = 1:degree
-            X[d, i] += x_vec[i]^(d)
+    X = zeros(T, (degree, n))
+    @inbounds for i in 1:n
+        for d in 1:degree
+            X[d, i] += x[i]^(d)
         end
     end
     X
 end
 
-function expand_poly{T<:Number}(x::AbstractVector{T}, degree::Integer, ::ObsDim.Constant{1})
+function expand_poly(x::AbstractVector{T}, degree::Integer, ::ObsDim.Constant{1}) where {T}
     n = length(x)
-    # FIXME: consider support for colorants (i.e. don't hardcode Float64)
-    x_vec = convert(Vector{Float64}, x)
-    X = zeros(Float64, (n, degree))
-    @inbounds for d = 1:degree
-        for i = 1:n
-            X[i, d] += x_vec[i]^(d)
+    X = zeros(T, (n, degree))
+    @inbounds for d in 1:degree
+        for i in 1:n
+            X[i, d] += x[i]^(d)
         end
     end
     X
--- test/tst_expand.jl
+++ test/tst_expand.jl
@@ -1,29 +1,50 @@
 @testset "expand_poly" begin
-    x = 1:6
-    @test_throws MethodError expand_poly(x, obsdim = 3)
-    @test_throws MethodError expand_poly(x, 5, ObsDim.Constant{3}())
+    xi = 1:6
+    xf = 1f0:6f0
+    @test_throws MethodError expand_poly(xi, obsdim = 3)
+    @test_throws MethodError expand_poly(xf, 5, ObsDim.Constant{3}())
 
     @testset "ObsDim.Last()" begin
-        X = expand_poly(x, degree = 5)
-        @test @inferred(expand_poly(x)) ≈ X
-        @test @inferred(expand_poly(x, 5)) ≈ X
-        @test @inferred(expand_poly(x, 5, ObsDim.Last())) ≈ X
-        @test @inferred(expand_poly(x, 5, ObsDim.Constant{2}())) ≈ X
-
-        @test size(X) == (5, 6)
-        for i in 1:6, k in 1:5
-            @test X[k, i] === Float64(x[i]^k)
+        for x in [xi, xf]
+            X = expand_poly(x, degree = 5)
+            @test eltype(x) == eltype(X)
+            @test @inferred(expand_poly(x)) == X
+            @test @inferred(expand_poly(x, 5)) == X
+            @test @inferred(expand_poly(x, 5, ObsDim.Last())) == X
+            @test @inferred(expand_poly(x, 5, ObsDim.Constant{2}())) == X
+            @test size(X) == (5, 6)
+            for i in 1:6, k in 1:5
+                @test X[k, i] === x[i]^k
+            end
         end
     end
 
     @testset "ObsDim.First()" begin
-        X = expand_poly(x, degree = 5, obsdim = 1)
-        @test expand_poly(x, obsdim = 1) ≈ X
-        @test @inferred(expand_poly(x, 5, ObsDim.First())) ≈ X
+        for x in [xi, xf]
+            X = expand_poly(x, degree = 5, obsdim = 1)
+            @test eltype(x) == eltype(X)
+            @test expand_poly(x, obsdim = 1) == X
+            @test @inferred(expand_poly(x, 5, ObsDim.First())) == X
+            @test size(X) == (6, 5)
+            for k in 1:5, i in 1:6
+                @test X[i, k] === x[i]^k
+            end
+        end
+    end
 
-        @test size(X) == (6, 5)
-        for k in 1:5, i in 1:6
-            @test X[i, k] === Float64(x[i]^k)
+    @testset "Missing" begin
+        xm = [1., 2., missing, 4., 5., 6.]
+        @test_throws MethodError expand_poly(xm, obsdim = 3)
+        @test_throws MethodError expand_poly(xm, 5, ObsDim.Constant{3}())
+        X = @inferred(expand_poly(xm, 5))
+        @test eltype(xm) == eltype(X)
+        @test size(X) == (5, 6)
+        for i in 1:6, k in 1:5
+            if i != 3
+                @test X[k, i] === xm[i]^k
+            else
+                @test ismissing(X[k, i])
+            end
         end
     end
 end
